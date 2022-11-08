2f24c26d383618c0524206d2c5e8e06f3c3ee915
yar1290 <Yarikm1290@gmail.com>
2022-11-08 12:39:29 +0200

    почав робити дз

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var input1 = document.getElementById("task1"),
+    res = document.getElementById("result"),
+    button = document.getElementById("button");
+    counter = 0;
 
+    button.addEventListener("click",
+    function (){
+        if (input1.value == "github"){
+            counter++
+            console.log(counter)
+            button.setAttribute("disabled", "true")
+            }
+            else{
+        alert("НЕПРАВИЛЬНО")
+            }
+    }
+)
+
+   
