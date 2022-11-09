1dc03d5098f57c4aa9de0bf7a62fee086cc18459
yar1290 <Yarikm1290@gmail.com>
2022-11-09 09:54:05 +0200

    зробив дз

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-var input1 = document.getElementById("task1"),
-    res = document.getElementById("result"),
-    button = document.getElementById("button");
-    counter = 0;
 
-    button.addEventListener("click",
-    function (){
-        if (input1.value == "github"){
-            counter++
-            console.log(counter)
-            button.setAttribute("disabled", "true")
-            }
-            else{
-        alert("НЕПРАВИЛЬНО")
-            }
-    }
-)
-
-   
